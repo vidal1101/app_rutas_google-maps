2d42ea79f16d86bd9c606f59d7fcce4ecf7f374a
vidal1101 <rodrigovdal278@gmail.com>
2022-11-18 16:31:43 -0600

    listen changes of geolocator and gps

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:app_googlemaps_rutas/blocs/blocs.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -16,5 +17,24 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
         isGpsPermissionGranted: event.isGpspermisionGranted,
       ))
     );
+    _init();
+  }
+
+  Future _init()async{
+    //permission = await Geo locator.checkPermission();
+    final isenabledstatus = await  _checkgpsStatus();
+    //print('$isenabledstatus');
+  }
+
+  Future _checkgpsStatus ()async {
+    final isenablegeolocator = await Geolocator.isLocationServiceEnabled();
+
+    Geolocator.getServiceStatusStream().listen((event) {
+      final isenabled = (event.index  ==1 ) ? true : false;
+      print('service status: ${isenabled}');
+
+    });
+    //print(isenable);
+    return isenablegeolocator;
   }
 }
